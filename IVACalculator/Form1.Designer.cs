336661171a4ef514e3e77d24ee137abc0f933992
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2021-09-29 17:13:54 +0200

    IVACalculator: v0.3 - Fix rounding della valuta (cast da rivedere) - Refactor del codice - New UI - Aggiunta valuta GBP - Supporto multi-lingua (Inglese, Italiano)

--- IVACalculator/Form1.Designer.cs
+++ IVACalculator/Form1.Designer.cs
@@ -24,8 +24,8 @@ namespace IVACalculator
         #region Codice generato da Progettazione Windows Form
 
         /// <summary>
-        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
-        /// il contenuto del metodo con l'editor di codice.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -37,28 +37,32 @@ namespace IVACalculator
             this.calcolaButton = new System.Windows.Forms.Button();
             this.versionText = new System.Windows.Forms.Label();
             this.currencyComboBox = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.valutaLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // prezzoTextBox
             // 
-            this.prezzoTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.prezzoTextBox.Location = new System.Drawing.Point(98, 44);
+            this.prezzoTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.prezzoTextBox.Location = new System.Drawing.Point(89, 18);
             this.prezzoTextBox.Name = "prezzoTextBox";
-            this.prezzoTextBox.Size = new System.Drawing.Size(57, 20);
+            this.prezzoTextBox.Size = new System.Drawing.Size(143, 20);
             this.prezzoTextBox.TabIndex = 0;
             this.prezzoTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.prezzoTextBoxKeyPress);
             // 
             // articoloText
             // 
-            this.articoloText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.articoloText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
             this.articoloText.AutoSize = true;
-            this.articoloText.Location = new System.Drawing.Point(110, 18);
+            this.articoloText.Location = new System.Drawing.Point(3, 21);
             this.articoloText.Name = "articoloText";
-            this.articoloText.Size = new System.Drawing.Size(77, 13);
+            this.articoloText.Size = new System.Drawing.Size(80, 13);
             this.articoloText.TabIndex = 1;
-            this.articoloText.Text = "Prezzo Articolo";
+            this.articoloText.Text = "Prezzo Articolo:";
             this.articoloText.Click += new System.EventHandler(this.articoloText_Click);
             // 
             // speseText
@@ -66,7 +70,7 @@ namespace IVACalculator
             this.speseText.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.speseText.AutoSize = true;
             this.speseText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.speseText.Location = new System.Drawing.Point(104, 133);
+            this.speseText.Location = new System.Drawing.Point(3, 8);
             this.speseText.Name = "speseText";
             this.speseText.Size = new System.Drawing.Size(83, 13);
             this.speseText.TabIndex = 2;
@@ -77,21 +81,21 @@ namespace IVACalculator
             this.speseFinaliText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.speseFinaliText.AutoSize = true;
             this.speseFinaliText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.speseFinaliText.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.speseFinaliText.Location = new System.Drawing.Point(133, 155);
+            this.speseFinaliText.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.speseFinaliText.Location = new System.Drawing.Point(85, 8);
             this.speseFinaliText.Name = "speseFinaliText";
             this.speseFinaliText.Size = new System.Drawing.Size(22, 13);
             this.speseFinaliText.TabIndex = 3;
             this.speseFinaliText.Text = "0,0";
-            this.speseFinaliText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.speseFinaliText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // calcolaButton
             // 
             this.calcolaButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.calcolaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.calcolaButton.Location = new System.Drawing.Point(98, 86);
+            this.calcolaButton.Location = new System.Drawing.Point(257, 140);
             this.calcolaButton.Name = "calcolaButton";
-            this.calcolaButton.Size = new System.Drawing.Size(101, 23);
+            this.calcolaButton.Size = new System.Drawing.Size(111, 23);
             this.calcolaButton.TabIndex = 4;
             this.calcolaButton.Text = "Calcola";
             this.calcolaButton.UseVisualStyleBackColor = true;
@@ -101,41 +105,93 @@ namespace IVACalculator
             // 
             this.versionText.AutoSize = true;
             this.versionText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.versionText.Location = new System.Drawing.Point(254, 196);
+            this.versionText.Location = new System.Drawing.Point(11, 150);
             this.versionText.Name = "versionText";
-            this.versionText.Size = new System.Drawing.Size(28, 13);
+            this.versionText.Size = new System.Drawing.Size(88, 13);
             this.versionText.TabIndex = 5;
-            this.versionText.Text = "v0.2";
+            this.versionText.Text = "SlimShadys, v0.3";
             // 
             // currencyComboBox
             // 
             this.currencyComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.currencyComboBox.FormattingEnabled = true;
-            this.currencyComboBox.Location = new System.Drawing.Point(161, 43);
+            this.currencyComboBox.Location = new System.Drawing.Point(89, 41);
             this.currencyComboBox.Name = "currencyComboBox";
             this.currencyComboBox.Size = new System.Drawing.Size(47, 21);
             this.currencyComboBox.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.valutaLabel);
+            this.panel1.Controls.Add(this.articoloText);
+            this.panel1.Controls.Add(this.prezzoTextBox);
+            this.panel1.Controls.Add(this.currencyComboBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(356, 82);
+            this.panel1.TabIndex = 7;
+            // 
+            // valutaLabel
+            // 
+            this.valutaLabel.Location = new System.Drawing.Point(3, 44);
+            this.valutaLabel.Name = "valutaLabel";
+            this.valutaLabel.Size = new System.Drawing.Size(76, 18);
+            this.valutaLabel.TabIndex = 7;
+            this.valutaLabel.Text = "Valuta:";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Informazioni";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.speseText);
+            this.panel2.Controls.Add(this.speseFinaliText);
+            this.panel2.Location = new System.Drawing.Point(12, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(356, 30);
+            this.panel2.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(294, 218);
-            this.Controls.Add(this.currencyComboBox);
-            this.Controls.Add(this.versionText);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(380, 172);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.calcolaButton);
-            this.Controls.Add(this.speseFinaliText);
-            this.Controls.Add(this.speseText);
-            this.Controls.Add(this.articoloText);
-            this.Controls.Add(this.prezzoTextBox);
+            this.Controls.Add(this.versionText);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "IVACalculator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Panel panel2;
+
+        private System.Windows.Forms.Label valutaLabel;
+
+        private System.Windows.Forms.Panel panel1;
 
         #endregion
 
--- IVACalculator/Form1.cs
+++ IVACalculator/Form1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Drawing;
+using System.Globalization;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Forms;
 
@@ -8,30 +10,38 @@ namespace IVACalculator
     public partial class Form1 : Form
     {
 
-        public static System.Net.WebClient wc = new System.Net.WebClient();
-        public static byte[] raw;
-        private string title = "Inserire una valuta!";
-        private string caption = "ATTENZIONE";
+        private readonly System.Net.WebClient wc = new System.Net.WebClient();
+        private CultureInfo currentCulture = CultureInfo.CurrentCulture;
+        private static byte[] raw;
+        private string title = Properties.Resources.ResourceManager.GetString("insertValue", CultureInfo.CurrentCulture);
+        private string caption = Properties.Resources.ResourceManager.GetString("msgCaption", CultureInfo.CurrentCulture);
         private double prezzoArticolo;
         private double prezzoFinale;
         private double currency;
-
+        private static String[] currencyList =
+        {
+            "CAD",
+            "EUR",
+            "GBP",
+            "USD"
+        };
+        
         public Form1()
         {
             InitializeComponent();
-            this.currencyComboBox.Items.Add("USD");
-            this.currencyComboBox.Items.Add("EUR");
-            this.currencyComboBox.Items.Add("CAD");
+            for (int i = 0; i < currencyList.Length; i++)
+            {
+                currencyComboBox.Items.Add(currencyList[i]);
+            }
             Aggiorna();
         }
 
         private void Aggiorna()
         {
             prezzoTextBox.BackColor = Color.White;
-            if (prezzoTextBox.Text == "0" || prezzoTextBox.Text == "")
+            if (isNotValid(prezzoTextBox.Text))
             {
-                speseFinaliText.Text = "Inserisci un prezzo!";
-                updateTextPosition();
+                speseFinaliText.Text = Properties.Resources.ResourceManager.GetString("insertPrice", currentCulture);
                 return;
             }
             try
@@ -42,16 +52,20 @@ namespace IVACalculator
                     return;
 
                 prezzoFinale = Math.Round(prezzoArticolo + (prezzoArticolo * 22 / 100) + 2.00 - prezzoArticolo, 2, MidpointRounding.AwayFromZero);
-                speseFinaliText.Text = "€ " + prezzoFinale.ToString();
+                speseFinaliText.Text = Properties.Resources.ResourceManager.GetString("euroValue", currentCulture) + prezzoFinale;
             }
             catch { }
-
-            updateTextPosition();
+            
         }
 
-        public double convert(String prezzo)
+        private static bool isNotValid(String str)
         {
-            currency = Math.Round(Double.Parse(getCurrency(this.currencyComboBox.Text.ToString())), 2, MidpointRounding.AwayFromZero);
+            return str == "0" || str == "" || Regex.IsMatch(str, ".*?[a-zA-Z].*?");
+        }
+
+        private double convert(String prezzo)
+        {
+            currency = Double.Parse(getCurrency(currencyComboBox.Text));
 
             if (currency == 0.00)
                 return 0.00;
@@ -59,31 +73,29 @@ namespace IVACalculator
             if (prezzo.Contains("."))
                 prezzo = prezzo.Replace(".", ",");
 
-            return double.Parse(prezzo) / currency;
+            return double.Parse(prezzo) / Math.Round(currency, 2, MidpointRounding.AwayFromZero);
         }
 
-        private string getCurrency(String currency)
+        private string getCurrency(String webCurrency)
         {
-            switch (currency)
+            switch (webCurrency)
             {
-                case "USD":
-                    raw = wc.DownloadData("https://free.currencyconverterapi.com/api/v6/convert?apiKey=57313b824a42f841f02c&q=EUR_USD&compact=y");
-                    break;
                 case "CAD":
-                    raw = wc.DownloadData("https://free.currencyconverterapi.com/api/v6/convert?apiKey=57313b824a42f841f02c&q=EUR_CAD&compact=y");
+                    raw = wc.DownloadData(Properties.Resources.ResourceManager.GetString("EURToCAD", currentCulture) ?? string.Empty);
                     break;
                 case "EUR":
                     return "1,00";
+                case "GBP":
+                    raw = wc.DownloadData(Properties.Resources.ResourceManager.GetString("EURToGBP", currentCulture) ?? string.Empty);
+                    break;
+                case "USD":
+                    raw = wc.DownloadData(Properties.Resources.ResourceManager.GetString("EURToUSD", currentCulture) ?? string.Empty);
+                    break;
                 default:
-                    MessageBoxResult result = System.Windows.MessageBox.Show(title, caption, MessageBoxButton.OK);
+                    System.Windows.MessageBox.Show(title, caption, MessageBoxButton.OK);
                     return "0,00";
             }
             return System.Text.Encoding.UTF8.GetString(raw).Substring(18, 7).Replace(".", ",");
-        }
-
-        private void updateTextPosition()
-        {
-            speseFinaliText.Left = (this.Width - speseFinaliText.Size.Width) / 2;
         }
 
         private void calcolaButton_Click(object sender, EventArgs e)
@@ -101,7 +113,7 @@ namespace IVACalculator
 
         private void articoloText_Click(object sender, EventArgs e)
         {
-            prezzoTextBox.Text = "dolito :P";
+            prezzoTextBox.Text = Properties.Resources.ResourceManager.GetString("easter", currentCulture);
             prezzoTextBox.BackColor = Color.Red;
         }
     }
